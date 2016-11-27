b87566e6464649d55ab5ba261f160b8706533d42
Nick Jordan <AussieGamersHD@Gmail.com>
2016-11-27 21:37:06 +1100

    Create TEFCP.cpp

---
+++ Community/TEFCP.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int Level;
+    cin >> Level; cin.ignore();
+    int Xp;
+    cin >> Xp; cin.ignore();
+    int N;
+    cin >> N; cin.ignore();
+    int CurXP = 0;
+
+    for(int i = 0; i < N; i++)
+    {
+        CurXP += 300;
+        while(CurXP >= Xp)
+        {
+            Level++;
+            CurXP -= Xp;
+            Xp = int( Level * sqrt(Level) * 10 );
+        }
+        
+    }
+    
+    cout << Level << endl;
+    cout << Xp-CurXP << endl;
+}
