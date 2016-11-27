87804d0cb8a2d468b2e50d448a199de63490fcb9
Nick Jordan <AussieGamersHD@Gmail.com>
2016-11-27 21:33:06 +1100

    Update Onboarding.cpp

--- Easy/Onboarding/Onboarding.cpp
+++ Easy/Onboarding/Onboarding.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-
-    // game loop
     while (1) {
-        std::string enemy1; // name of enemy 1
-        std::cin >> enemy1; std::cin.ignore();
+        string enemy1; // name of enemy 1
+        cin >> enemy1; cin.ignore();
         int dist1; // distance to enemy 1
-        std::cin >> dist1; std::cin.ignore();
-        std::string enemy2; // name of enemy 2
-        std::cin >> enemy2; std::cin.ignore();
+        cin >> dist1; cin.ignore();
+        string enemy2; // name of enemy 2
+        cin >> enemy2; cin.ignore();
         int dist2; // distance to enemy 2
-        std::cin >> dist2; std::cin.ignore();
+        cin >> dist2; cin.ignore();
 
-        // Enter the code here
-        
-        if (dist1 < dist2) {
-            std::cout << enemy1 << std::endl;
-        }  else {
-            std::cout << enemy2 << std::endl;
-        }
+        if (dist1 < dist2)
+            cout << enemy1 << endl;
+        else
+            cout << enemy2 << endl;
     }
 }
